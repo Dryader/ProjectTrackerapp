ee5ca27bb9e631f3d75c27767427dcba11ede7cc
Yuval <.>
2024-02-22 14:46:18 -0500

    Project instance created

--- ProjectTrackerApp/MainPage.xaml.cs
+++ ProjectTrackerApp/MainPage.xaml.cs
@@ -4,9 +4,20 @@ namespace ProjectTrackerApp;
 
 public partial class MainPage : ContentPage
 {
+    private readonly ProjectRepository _repository = new();
+
     public MainPage()
     {
         InitializeComponent();
-        new ProjectRepository();
+        //new ProjectRepository();
+        //ProjectRepository _repository = new ProjectRepository();
+    }
+
+    public void AddButton_OnClicked(object sender, EventArgs e)
+    {
+        //_repository.AddProject(Project e);
+        //ProjectsListView.
+        SummaryLabel.Text = _repository.TotalCostDeviation().ToString();
+        DisplayAlert("Success", "Project was added", "Ok");
     }
 }
